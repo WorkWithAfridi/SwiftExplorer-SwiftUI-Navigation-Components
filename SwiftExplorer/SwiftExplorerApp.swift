1b224a4e5e60284e15f6c97c5f9ae9dcc1d17914
khondakar.afridi <afridi.khondakar@gmail.com>
2023-12-27 08:24:52 +0600

    Initial Commit

---
+++ SwiftExplorer/SwiftExplorerApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftExplorerApp.swift
+//  SwiftExplorer
+//
+//  Created by Khondakar Afridi on 27/12/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftExplorerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
