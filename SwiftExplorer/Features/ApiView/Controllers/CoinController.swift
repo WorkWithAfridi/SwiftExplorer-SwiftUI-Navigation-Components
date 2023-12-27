a1c5b552dfc5a5f6bb44dcbc7c54467b59bdb876
khondakar.afridi <afridi.khondakar@gmail.com>
2023-12-27 19:34:52 +0600

    apiView - GetCoin integrated

--- SwiftExplorer/Features/ApiView/Controllers/CoinController.swift
+++ SwiftExplorer/Features/ApiView/Controllers/CoinController.swift
@@ -8,5 +8,41 @@
 import Foundation
 
 class CoinController : ObservableObject{
-    var coinList = [CoinModel]()
+    @Published var coinList = [CoinModel]()
+    @Published var errorMessage: String?
+    
+    private var coinService = CoinServices()
+    
+    func getCoinsAsync() async throws {
+        errorMessage = nil
+        let result: Result = try await coinService.fetchCoinListAsync()
+        
+        switch result {
+        case .success(let coins):
+            DispatchQueue.main.async{
+                self.coinList = coins
+            }
+        case .failure(let failure):
+            DispatchQueue.main.async{
+                self.errorMessage = failure.localizedDescription
+            }
+        }
+    }
+    
+    func getCoinUsingCompletionHandler() {
+        errorMessage = nil
+        coinService.getCoinList { apiCoinList, error in
+            if let errorNotNil = error {
+                DispatchQueue.main.async{
+                    self.errorMessage = errorNotNil.localizedDescription
+                }
+            }
+            if let safeCoinList = apiCoinList {
+                DispatchQueue.main.async{
+                    self.coinList = safeCoinList
+                }
+            }
+        }
+    }
 }
+
--- SwiftExplorer/Features/ApiView/Views/CoinListView.swift
+++ SwiftExplorer/Features/ApiView/Views/CoinListView.swift
@@ -11,25 +11,40 @@ struct CoinListView: View {
     @StateObject private var coinController = CoinController()
     var body: some View {
         VStack{
-            if coinController.coinList.isEmpty {
+            if coinController.errorMessage != nil {
+                ContentUnavailableView("404", systemImage: "globe", description: Text(coinController.errorMessage ?? "An error occurred when trying to fetch coin data.") )
+            }
+            else if coinController.coinList.isEmpty {
                 VStack{
                     Button {
-                        
+                        Task{
+                            try await coinController.getCoinsAsync()
+                        }
                     } label: {
                         Text("Get Coin using Async/Await")
                     }
                     Divider()
                     
                     Button {
-                        
+                        coinController.getCoinUsingCompletionHandler()
                     } label: {
                         Text("Get Coin using Completion Handler")
                     }
                     
                 }
             }
+            else if !coinController.coinList.isEmpty{
+                List {
+                    ForEach(coinController.coinList, id: \.id){ coin in
+                        Text(coin.name ?? "NO-COIN-NAME")
+                    }
+                }
+            }
         }
         .onAppear(perform: {
+            //            Task{
+            //                try await coinController.getCoinsAsync()
+            //            }
             print("CoinListView Appeared")
         })
         .onDisappear(perform: {
